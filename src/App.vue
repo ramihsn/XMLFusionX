3b2e673df7b0493ee2021c83f87dfada0e37c335
Rami Hassan <hsn.rami@gmail.com>
2024-07-20 20:40:13 +0300

    make main screen and header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
-<script setup>
-</script>
-
 <template>
-  <h1>
-    Hello, world!
-  </h1>
+  <header>
+    <Header />
+  </header>
+  <main>
+    <Main />
+  </main>
 </template>
 
+<script setup>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+</script>
+
 <style scoped>
+header {
+  background-color: #f0f0f0;
+  border-bottom: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.header-content {
+  padding: 1rem;
+}
+
+main {
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>
+        {{ author }} says: {{ title }}
+    </h1>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    setup () {
+        return {
+            author: 'John Doe',
+            title: 'Hello, world!'
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
