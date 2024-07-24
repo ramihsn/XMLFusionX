7be1132f77ad29f4766965438165379f7e77be11
Rami Hassan <hsn.rami@gmail.com>
2024-07-24 23:33:24 +0300

    restore font-awesome faCirclePlus

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCircleInfo, faUserTie, faCloudArrowDown, faCaretDown, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faCircleInfo, faUserTie, faCloudArrowDown, faCaretDown, faSpinner, faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faFileCode, faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(
     faCircleInfo, faUserTie, faLinkedin, faEnvelope, faGithub,
-    faFileCode, faTrashCan, faCloudArrowDown,
+    faFileCode, faTrashCan, faCloudArrowDown, faCirclePlus,
     faCaretDown, faSpinner,
 );
 
